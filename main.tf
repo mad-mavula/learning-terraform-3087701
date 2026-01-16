4a8a7567c88f4f62b47e38bb2caeeeb8fb7518cd
Manula Manjitha <59197160+mad-mavula@users.noreply.github.com>
2026-01-16 11:29:34 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,3 @@
-# data "aws_ami" "app_ami" {
-#   most_recent = true
-# 
-#   filter {
-#     name   = "name"
-#     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-#   }
-# 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
-# 
-#   owners = ["979382823631"] # Bitnami
-# }
-# 
-# resource "aws_instance" "web" {
-#   ami           = data.aws_ami.app_ami.id
-#   instance_type = "t2.micro"
-# 
-#   tags = {
-#     Name = "HelloWorld"
-#   }
-# }
-
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"]  # Canonical's ID
