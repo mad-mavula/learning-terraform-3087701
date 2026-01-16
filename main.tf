8d214a4838d70c0ad7dbbcca66d7f9d960dd42a5
Manula Manjitha <59197160+mad-mavula@users.noreply.github.com>
2026-01-16 11:25:53 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,46 @@
-data "aws_ami" "app_ami" {
+# data "aws_ami" "app_ami" {
+#   most_recent = true
+# 
+#   filter {
+#     name   = "name"
+#     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+#   }
+# 
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+# 
+#   owners = ["979382823631"] # Bitnami
+# }
+# 
+# resource "aws_instance" "web" {
+#   ami           = data.aws_ami.app_ami.id
+#   instance_type = "t2.micro"
+# 
+#   tags = {
+#     Name = "HelloWorld"
+#   }
+# }
+
+data "aws_ami" "ubuntu" {
   most_recent = true
+  owners      = ["099720109477"]  # Canonical's ID
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
-  owners = ["979382823631"] # Bitnami
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t2.micro"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"  # Free Tier
 
   tags = {
     Name = "HelloWorld"
